a2c88c6393c6f3f76aefd1abe0227e9d9d8f57ed
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2021-09-30 19:45:07 +0530

    easy for each

--- index-Js.js
+++ index-Js.js
@@ -1,5 +1,10 @@
 var arr =[1,2,3]
-var callback=()=>{
-    console.log('I ran!')
+var callback=(elmt,index)=>{
+    console.log(elmt,index)
 }
-arr.forEach(callback)
+arr.forEach(callback);
+
+var arr1 =[10,20,30]
+arr1.forEach((elmtt,index1) => {
+    console.log(elmtt,index1)
+})
