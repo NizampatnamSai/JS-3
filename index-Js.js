9dcec2cff5958b98bb51eebc9738b5e67a46dab1
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2021-09-30 19:32:38 +0530

    For each()

--- index-Js.js
+++ index-Js.js
@@ -1,23 +1,5 @@
-function add(x,y){
-    z=x+y;
-    console.log(z)
+var arr =[1,2,3]
+var callback=()=>{
+    console.log('I ran!')
 }
-add(4,1)
-var add=function(x=1,y=2){
-           z=x+y;
-           console.log(z)
-}
-add(4,5)
-var add=(x=3,y=5)=>{
-    z=x+y;
-    console.log(z)
-}
-add(65,65)
-var h2=document.getElementById('h2');
-var h21=h2.innerHTML;
-h21=56;
-h2.innerHTML=h21;
-var add=(a=2,b=3)=>a+b;
-console.log(add(4,5));
-var date=day => day+'th';
-console.log(date(2))
+arr.forEach(callback)
