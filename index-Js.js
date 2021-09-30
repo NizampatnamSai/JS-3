ede5fcf746c220174ce9c99080eb3acc7e7414d0
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2021-09-30 19:03:58 +0530

    function expression

--- index-Js.js
+++ index-Js.js
@@ -1,5 +1,10 @@
-function add(x=2,y=3){
+function add(x,y){
     z=x+y;
     console.log(z)
 }
-add()
+add(4,1)
+var add=function(x=1,y=2){
+           z=x+y;
+           console.log(z)
+}
+add(4,5)
